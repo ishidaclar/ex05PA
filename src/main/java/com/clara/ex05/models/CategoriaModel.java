a6768c45f077244e8e260294942291a7f2a2a005
ishidaclar <cishidadias@gmail.com>
2026-03-22 22:24:28 -0300

    models

---
+++ src/main/java/com/clara/ex05/models/CategoriaModel.java
@@ -0,0 +1,43 @@
+package com.clara.ex05.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="tb_categoria")
+
+public class CategoriaModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "Nome da categoria")
+    private String nome;
+    @Column(name = "Descrição")
+    private String descricao;
+
+    public CategoriaModel(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
